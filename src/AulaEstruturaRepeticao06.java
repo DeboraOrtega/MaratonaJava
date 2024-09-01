1ecee4d7b2b92e442b9c427e64dcf333964b99d4
Debora Ortega <debora.tpd@gmail.com>
2024-09-01 16:39:02 -0300

    atualizacao local para remoto

--- src/AulaEstruturaRepeticao06.java
+++ src/AulaEstruturaRepeticao06.java
@@ -1,2 +1,23 @@
 public class AulaEstruturaRepeticao06 {
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        //break
+        System.out.println("break");
+        for (int parcela = 1; parcela <= valorTotal; parcela++){
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela <= 1000){
+                break;
+            }
+            System.out.println("Parcela: " +parcela + " R$" + valorParcela);
+        }
+        //continue
+        System.out.println("continue");
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--){
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela <= 1000){
+                continue;
+            }
+            System.out.println("Parcela: " +parcela + " R$" + valorParcela);
+        }
+    }
 }
