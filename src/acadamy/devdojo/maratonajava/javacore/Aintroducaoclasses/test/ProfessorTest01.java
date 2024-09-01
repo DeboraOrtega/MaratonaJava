8c06d181b20e2dc250ab75b0339aac7aa7c6f7bf
Debora Ortega <debora.tpd@gmail.com>
2024-09-01 19:21:59 -0300

    Aula Introducao Classe 03

---
+++ src/acadamy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package acadamy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import acadamy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Mestre Kami";
+        professor.idade = 60;
+        professor.sexo = 'M';
+
+        System.out.println("nome: " + professor.nome + " idade: " + professor.idade + " sexo:" + professor.sexo);
+    }
+}
