17bcebd47f3470310fed740a8c804b39cab0f65b
Debora Ortega <debora.tpd@gmail.com>
2024-09-02 00:38:21 -0300

    Até a aula Metodos 04

--- src/acadamy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/acadamy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -7,4 +7,21 @@ public class Calculadora {
     public void subtraiDoisNumeros(){
         System.out.println(20-5);
     }
+    public void multiplicaDoisNumero(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+
+    public double divideDoisNumeros(int num1, int num2){
+        if (num2 == 0){
+            return 0;
+        }
+        return num1/num2;
+    }
+    public void imprimeDividsaoDeDoisNumeros(int num1, int num2){
+        if (num2 ==0){
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+        System.out.println(num1/num2);
+    }
 }
